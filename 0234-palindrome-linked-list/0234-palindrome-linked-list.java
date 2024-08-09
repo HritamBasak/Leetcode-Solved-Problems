1f039e3c3aa666296b037aae66342321d6f0e0bb
Hritam Basak <137753891+HritamBasak@users.noreply.github.com>
2024-08-10 00:06:52 +0530

    Time: 3 ms (99.68%), Space: 68.7 MB (24.44%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -12,38 +12,64 @@ class Solution
 {
     public boolean isPalindrome(ListNode head) 
     {
-    ListNode copy = new ListNode(head.val);
-    ListNode tempCopy = copy;
-    ListNode tempOriginal = head.next;
+        // Method 1:Firstly by t=copying the list and reversing the list
+    // ListNode copy = new ListNode(head.val);
+    // ListNode tempCopy = copy;
+    // ListNode tempOriginal = head.next;
 
-    while (tempOriginal != null) 
+    // while (tempOriginal != null) 
+    // {
+    // tempCopy.next = new ListNode(tempOriginal.val);
+    // tempCopy = tempCopy.next;
+    // tempOriginal = tempOriginal.next;
+    // }
+
+    // // Now reverse the original list
+    // ListNode temp = head;
+    // ListNode prev = null;
+    // while (temp != null) 
+    // {
+    // ListNode cur = temp.next;
+    // temp.next = prev;
+    // prev = temp;
+    // temp = cur;
+    // }
+
+    // // Compare the copied list with the reversed list
+    // while (prev != null && copy != null) 
+    // {
+    // System.out.println(copy.val + " " + prev.val);
+    // if (copy.val != prev.val)
+    //     return false;
+    // copy = copy.next;
+    // prev = prev.next;
+    // }   
+    // return true;
+
+    //Method 2:By slow and fast pointers
+    ListNode slow=head;
+    ListNode fast=head;
+    while(fast!=null && fast.next!=null)
     {
-    tempCopy.next = new ListNode(tempOriginal.val);
-    tempCopy = tempCopy.next;
-    tempOriginal = tempOriginal.next;
+        slow=slow.next;
+        fast=fast.next.next;
     }
-
-    // Now reverse the original list
-    ListNode temp = head;
-    ListNode prev = null;
-    while (temp != null) 
+    ListNode prev = null, curr = slow;
+    while (curr != null)
     {
-    ListNode cur = temp.next;
-    temp.next = prev;
-    prev = temp;
-    temp = cur;
+        ListNode next1 = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next1;
     }
-
-    // Compare the copied list with the reversed list
-    while (prev != null && copy != null) 
+    ListNode secondhalf=prev;
+    while(prev!=null)
     {
-    System.out.println(copy.val + " " + prev.val);
-    if (copy.val != prev.val)
-        return false;
-    copy = copy.next;
-    prev = prev.next;
-    }   
+        if(prev.val!=head.val)
+            return false;
+        prev=prev.next;
+        head=head.next;
+    }
     return true;
-
     }
 }
