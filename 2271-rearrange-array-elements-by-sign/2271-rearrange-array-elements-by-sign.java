cbe17190f40a1ac8e55b08d349b6381e13c5c87d
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-02-16 11:58:55 +0530

    Time: 3 ms (100%), Space: 76.7 MB (86.57%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,27 +1,46 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        ArrayList<Integer> list1=new ArrayList<>();
+        // ArrayList<Integer> list=new ArrayList<>();
+        // ArrayList<Integer> list1=new ArrayList<>();
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]>0)
+        //     list.add(nums[i]);
+        //     else
+        //     list1.add(nums[i]);
+        // }
+        // int ans[]=new int[nums.length];
+        // int a=0,b=0;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(i%2==0)
+        //     {
+        //         ans[i]=list.get(a++);
+        //     }
+        //     else
+        //     {
+        //         ans[i]=list1.get(b++);
+        //     }
+        // }
+        // return ans;
+        int odd=-1;
+        int even=-2;
+        int arr[]=new int[nums.length];
         for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]>0)
-            list.add(nums[i]);
-            else
-            list1.add(nums[i]);
-        }
-        int ans[]=new int[nums.length];
-        int a=0,b=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(i%2==0)
+            if(nums[i]>0)//even
             {
-                ans[i]=list.get(a++);
+                int temp=nums[i];
+                arr[even+2]=temp;
+                even=even+2;//4
             }
             else
             {
-                ans[i]=list1.get(b++);
+                int temp=nums[i];
+                arr[odd+2]=temp;
+                odd=odd+2;//1
             }
         }
-        return ans;
+        return arr;
     }
 }
