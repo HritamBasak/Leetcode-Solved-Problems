3761748e1e5b8d7050eee356ce65fa23cd6d7ef4
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-06-12 20:17:54 +0530

    Time: 15 ms (39.24%), Space: 48.6 MB (60.73%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,40 +2,26 @@ class Solution
 {
     public int[] topKFrequent(int[] nums, int k) 
     {
-      Map<Integer,Integer> map=new HashMap<>();
-      for(int i=0;i<nums.length;i++)
-      {
-        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-      }
-      Map<Integer,ArrayList<Integer>> m1=new HashMap<>();
-      for(Map.Entry<Integer,Integer> e:map.entrySet())
-      {
-        ArrayList<Integer> list1=new ArrayList<>();
-        if(m1.get(e.getValue())==null)
-        {
-            list1.add(e.getKey());
-            m1.put(e.getValue(),list1);
+      Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        else
-        {
-            list1=m1.get(e.getValue());
-            list1.add(e.getKey());
-            m1.put(e.getValue(),list1);
+
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
+            new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            minHeap.offer(entry);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
         }
-      }
-      //System.out.print(m1);
-      ArrayList<Integer> list=new ArrayList<>();
-      for(Map.Entry<Integer,ArrayList<Integer>> m:m1.entrySet())
-      {
-        list.addAll(m.getValue());
-      }
-      int arr[]=new int[k];
-      int i=0;
-      while(i<k)
-      {
-        arr[i]=list.get(list.size()-i-1);
-        i++;
-      }
-      return arr;
+
+        int[] result = new int[k];
+        int i = 0;
+        while (!minHeap.isEmpty()) {
+            result[i++] = minHeap.poll().getKey();
+        }
+        return result;
     }
 }
