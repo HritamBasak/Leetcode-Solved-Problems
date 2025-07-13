d74300e15d3395c34c5cb978b38c2783226545ed
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-07-13 20:13:40 +0530

    Time: 1 ms (99.82%), Space: 47.1 MB (66.02%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,19 +1,54 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int l=0;
-        int r=numbers.length-1;
-        while(l<r)
+        // List<Integer> list = new ArrayList<>();
+        // for (int i : numbers) {
+        //     list.add(i);
+        // }
+        // Set<Integer> set = new LinkedHashSet<>(); 
+        // //1 2 3 4 4 
+        // //target: 8
+        // for (int i = 0; i < numbers.length - 1; i++) {
+        //     int diff = target - numbers[i];
+
+        //     // Check for distinct elements forming the sum
+        //     if (list.contains(diff) && numbers[i] != diff) {
+        //         set.add(i + 1); 
+        //         set.add(list.indexOf(diff) + 1);
+        //         break; 
+        //     }
+        //     if (numbers[i] == numbers[i + 1] && numbers[i] * 2 == target) {
+        //         set.add(i + 1);
+        //         set.add(i + 2);
+        //         break;
+        //     }
+        // }
+        // int[] a = new int[2];
+        // int j = 0;
+        // for (int i : set) {
+        //     a[j] = i;
+        //     j++;
+        // }
+        // return a;
+
+        int arr[]=new int[2];
+        int left=0;
+        int right=numbers.length-1;
+        while(left<right)
         {
-            int sum=numbers[l]+numbers[r];
+            int sum=numbers[left]+numbers[right];
             if(sum==target)
-            return new int[]{l+1,r+1};
-
-            else if(sum<target)
-            l++;
-
+            {
+                arr[0]=left+1;
+                arr[1]=right+1;
+                break;
+            }
+            if(sum>target)
+            {
+                right--;
+            }
             else
-            r--;
+            left++;
         }
-        return new int[]{0,0};
+        return arr;
     }
 }
