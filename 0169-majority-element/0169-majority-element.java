5b6f1320e51b0d05428887ce2a1e5b5f6fa8204a
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-07-14 17:29:13 +0530

    Time: 13 ms (29.37%), Space: 49.4 MB (86.59%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -5,12 +5,11 @@ class Solution {
         {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        int max=0;
         for(int i:map.keySet())
         {
-            if(map.get(i)>max && map.get(i)>(nums.length/2))
-            max=i;
+            if(map.get(i)>(nums.length/2))
+            return i;
         }
-        return max;
+        return -1;
     }
 }
