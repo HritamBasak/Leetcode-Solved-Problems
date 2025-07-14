c4c38f4ab8379e8935b653e5555f8b49d4174824
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-07-14 17:53:24 +0530

    Time: 1 ms (99.85%), Space: 53.1 MB (59.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // for(int i:map.keySet())
+        // {
+        //     if(map.get(i)>(nums.length/2))
+        //     return i;
+        // }
+        // return -1;
+        
+        //Moore's Voting Algo
+        int ele=nums[0];
+
+        int count=0;
+        for(int i=0;i<nums.length-1;i++)
         {
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            if(nums[i]==ele)
+            {
+                count++;
+            }
+            else
+            count--;
+
+            if(count==0)
+            ele=nums[i+1];
         }
-        for(int i:map.keySet())
-        {
-            if(map.get(i)>(nums.length/2))
-            return i;
-        }
-        return -1;
+        return ele;
     }
 }
