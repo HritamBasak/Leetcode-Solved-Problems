8995ce732078b845af01827bb1046ee58728ac08
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-08-03 18:02:15 +0530

    Time: 7 ms (12.84%), Space: 48.9 MB (28.84%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -13,7 +13,7 @@ public class Solution
 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
     {   
-          //O(M*N) solution
+          //Brute Force solution
         //   ListNode start=headB;
         //   while(headA!=null)
         //   {
@@ -28,22 +28,62 @@ public class Solution
         //     headA=headA.next;
         //   }
         //   return null;
-          Map<ListNode,Integer> map=new LinkedHashMap<>();
-          while(headA!=null)
-          {
-            map.put(headA,map.getOrDefault(headA,0)+1);
+
+        //Better Solution
+        //   Map<ListNode,Integer> map=new LinkedHashMap<>();
+        //   while(headA!=null)
+        //   {
+        //     map.put(headA,map.getOrDefault(headA,0)+1);
+        //     headA=headA.next;
+        //   }
+        //   while(headB!=null)
+        //   {
+        //     map.put(headB,map.getOrDefault(headB,0)+1);
+        //     headB=headB.next;
+        //   }
+        //   for(ListNode i:map.keySet())
+        //   {
+        //     if(map.get(i)==2)
+        //     return i;
+        //   }
+        //   return null;
+
+        //Optimal Solution
+        int l1=0,l2=0;
+        ListNode first=headA,second=headB;
+        while(headA!=null)
+        {
+            l1++;
             headA=headA.next;
-          }
-          while(headB!=null)
-          {
-            map.put(headB,map.getOrDefault(headB,0)+1);
+        }
+        while(headB!=null)
+        {
+            l2++;
             headB=headB.next;
-          }
-          for(ListNode i:map.keySet())
-          {
-            if(map.get(i)==2)
-            return i;
-          }
-          return null;
+        }
+        int diff=Math.abs(l2-l1);
+        if(l1>l2)
+        {
+            for(int i=0;i<diff;i++)
+            {
+                first=first.next;
+            }
+        }
+        if(l2>l1)
+        {
+            for(int i=0;i<diff;i++)
+            {
+                second=second.next;
+            }
+        }
+        System.out.println(first.val+" "+second.val);
+        while(first!=null || second!=null)
+        {
+            if(first==second)
+            return first;
+            first=first.next;
+            second=second.next;
+        }
+        return null;
     }
 }
