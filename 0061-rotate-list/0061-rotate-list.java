15bac43e651c23f816ea3ea2544bdc9fde0eb8aa
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-08-06 11:50:37 +0530

    Time: 0 ms (100%), Space: 42.4 MB (79.87%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -8,47 +8,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution 
-{
-    public ListNode rotateRight(ListNode head, int k) 
-    {
-        
-
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null) return null;
         int c=0;
         ListNode temp=head;
+        ListNode ans=head;
         while(temp!=null)
         {
             c++;
             temp=temp.next;
         }
-        if(c==1 || c==0)
-        return head;
         if(k>=c)
         k=k%c;
 
-        if(head==null && k==0)
-        return null;
-
-        if(head==null)
-        return null;
-
-        if(k==0)
-        return head;
-        ListNode temp1=head;
-        ListNode top=temp1;
-        for(int i=0;i<(c-k)-1;i++)
+        ListNode dummy=new ListNode(-1);
+        ListNode finalAns=dummy;
+        for(int i=0;i<c-k-1;i++)
         {
-            temp1=temp1.next;
+            head=head.next;
         }
-        ListNode t=temp1.next;
-        ListNode ans=t;
-        temp1.next=null;
-        while(t.next!=null)
+        ListNode res=head.next;
+        head.next=null;
+        dummy.next=res;
+        while(dummy.next!=null)
         {
-            t=t.next;
+            dummy=dummy.next;
         }
-        t.next=top;
-
-        return ans;
+        dummy.next=ans;
+        return finalAns.next;
     }
 }
