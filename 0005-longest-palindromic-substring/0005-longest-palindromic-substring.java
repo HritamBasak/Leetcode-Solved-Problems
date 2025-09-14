c8652014001bbd0ffe8996e9e76ec49684baf4c6
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-09-14 23:44:26 +0530

    Time: 21 ms (43.66%), Space: 45.4 MB (31.64%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,51 +1,43 @@
-class Solution 
-{   
-  public String longestPalindrome(String s) 
-  {
-    if(s.length()<=1)
-    return s;
-      String str="";
-	  String max_str="";
-	  int max=0;
-	  for(int i=1;i<s.length();i++) 
-	  {
-        //For odd length
-		  int low=i;
-		  int high=i;
-		  
-		  while(s.charAt(low)==s.charAt(high))
-		  {  
-			  low--;
-			  high++;
-			  
-			  if(low==-1 || high==s.length())
-				  break;
-		  }
-			  str=s.substring(low+1,high);
-			  if(str.length()>=max)
-			  {
-				  max=str.length();
-				  max_str=str;//BAB
-			  }
-		  //For even length
-		  low=i-1;
-		  high=i;
-		  
-		  while(s.charAt(low)==s.charAt(high))
-		  {
-			  low--;
-			  high++;
-			  
-			  if(low==-1 || high==s.length())
-				  break;
-		  }
-			  str=s.substring(low+1,high);
-			  if(str.length()>max)
-			  {
-				  max=str.length();
-				  max_str=str;
-			  }
-	  }
-    return max_str;
-  }
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s.length()<=1)
+        return s;
+        int max=0;
+        String res="";
+        String maxStr="";
+        for(int i=1;i<s.length();i++)
+        {
+            int low=i;
+            int high=i;
+            while(s.charAt(low)==s.charAt(high))
+            {
+                    low--;
+                    high++;
+                    if(low<=-1 || high>=s.length())
+                    break; 
+            }
+            res=s.substring(low+1,high);
+            if(res.length()>max)
+            {
+                max=Math.max(res.length(),max);
+                maxStr=res;
+            }
+            low=i-1;
+            high=i;
+            while(s.charAt(low)==s.charAt(high))
+            {
+                    low--;
+                    high++;
+                    if(low<=-1 || high>=s.length())
+                    break;
+            }
+            res=s.substring(low+1,high);
+            if(res.length()>max)
+            {
+                max=Math.max(res.length(),max);
+                maxStr=res;
+            }
+        }
+        return maxStr;
+    }
 }
