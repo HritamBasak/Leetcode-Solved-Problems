be8dc63eb9316e89c49e068b91b6a76c5074ad3d
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-09-17 23:34:11 +0530

    Time: 3 ms (90.12%), Space: 43 MB (95.6%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,32 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int a[]=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++)
-        {
-            a[i]=-1;
+        int res[] = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i] = -1;
         }
-        List<Integer> list1=new ArrayList<>();
-        List<Integer> list2=new ArrayList<>();
 
-        for(int i:nums1)
-        list1.add(i);//4,1,2
-
-        for(int i:nums2)
-        list2.add(i);//1,3,4,2
-
-        for(int i=0;i<nums1.length;i++)//4,1,2
-        {
-            int num=nums1[i];//4
-                int index=list2.indexOf(num);//1
-                for(int j=index;j<list2.size()-1;j++)
-                {
-                    if(list2.get(index)<list2.get(j+1))
-                    {
-                    a[list1.indexOf(num)]=list2.get(j+1);
+        for(int i=0;i<nums1.length;i++){
+            int k=0;
+            while(nums1[i]!=nums2[k]){
+                k++;
+            }
+            for(int j=k;j<nums2.length;j++){
+                if(nums1[i]<nums2[j]){
+                    res[i] = nums2[j];
                     break;
-                    }
                 }
+            }
         }
-        return a;
+        return res;
     }
 }
