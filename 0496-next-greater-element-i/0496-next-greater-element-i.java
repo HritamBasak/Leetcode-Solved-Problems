dbfbf2d2f9c1ad301f1dc36348fd436cebeae056
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2025-09-17 23:17:16 +0530

    Time: 32 ms (6.07%), Space: 43.9 MB (56.4%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,66 +1,32 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        // int arr[]=new int[nums1.length];
-        // for(int i=0;i<nums1.length;i++)
-        // {
-        //     arr[i]=0;
-        // }
-        // ArrayList<Integer> list=new ArrayList<>();
-        // for(int i:nums1)
-        // {
-        //     list.add(i);
-        // }
-        // ArrayList<Integer> list1=new ArrayList<>();
-        // for(int i:nums2)
-        // {
-        //     list1.add(i);
-        // }
-        // for(int i=0;i<list.size();i++)
-        // {
-        //     int num=list.get(i);//1
-        //     int index=list1.indexOf(num);//0
-        //     int temp=index;//0
-        //     while(temp<list1.size()-1)
-        //     {
-        //         if(list1.get(temp+1)>=list1.get(index))
-        //         {
-        //             arr[i]=list1.get(temp+1);
-        //             break;
-        //         }
-        //         temp++;
-        //     }
-        //     if(arr[i]==0)
-        //     arr[i]=-1;
-        // }
-        // return arr;
-        int res[]=new int[nums1.length];
-        int n=nums2.length;
-        List<Integer> list=new ArrayList<>();
-        Stack<Integer> st=new Stack<>();
+        int a[]=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++)
+        {
+            a[i]=-1;
+        }
+        List<Integer> list1=new ArrayList<>();
+        List<Integer> list2=new ArrayList<>();
+
         for(int i:nums1)
+        list1.add(i);//4,1,2
+
+        for(int i:nums2)
+        list2.add(i);//1,3,4,2
+
+        for(int i=0;i<nums1.length;i++)//4,1,2
         {
-            list.add(i);
+            int num=nums1[i];//4
+                int index=list2.indexOf(num);//1
+                for(int j=index;j<list2.size()-1;j++)
+                {
+                    if(list2.get(index)<list2.get(j+1))
+                    {
+                    a[list1.indexOf(num)]=list2.get(j+1);
+                    break;
+                    }
+                }
         }
-        //list:4 1 2
-        for(int i=n-1;i>=0;i--)
-        {
-            if(list.contains(nums2[i]))
-            {
-                while(!st.isEmpty() && nums2[i]>=nums2[st.peek()])
-                {
-                    st.pop();
-                }
-                if(!st.isEmpty())
-                {
-                    res[list.indexOf(nums2[i])]=nums2[st.peek()];
-                }
-                else
-                {
-                    res[list.indexOf(nums2[i])]=-1;
-                }
-            }
-            st.push(i);
-        }
-        return res;
+        return a;
     }
 }
