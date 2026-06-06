5d6859d9a9c44d28d2cc5659b5e7bb853f4fe96d
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2026-06-06 21:28:49 +0530

    Time: 389 ms (5.15%), Space: 48 MB (6.72%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,48 +1,24 @@
-class Solution 
-{
-    public static Map<Character,Integer> sort(Map<Character,Integer> map2)
-	{
-		//Converting map into a list
-		List<Map.Entry<Character,Integer>> list=new ArrayList<>(map2.entrySet());
-		
-		//Soting the list
-		Collections.sort(list,new Comparator<Map.Entry<Character,Integer>>()
-				{
-			         public int compare(Map.Entry<Character,Integer> o1,Map.Entry<Character,Integer> o2)
-			         {
-			        	 return o1.getValue().compareTo(o2.getValue());
-			         }
-				});
-		Map<Character,Integer> m1=new LinkedHashMap<>();
-		for(Map.Entry<Character,Integer> m:list)
-		{
-			m1.put(m.getKey(),m.getValue());
-		}
-		
-		return m1;
-	}
-    public String frequencySort(String s) 
-    {
-     Map<Character,Integer> map=new HashMap<>();
-	  for(int i=0;i<s.length();i++)
-	  {
-		  map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-	  }
-
-       Map<Character,Integer> map1=sort(map);
-
+class Solution {
+    public String frequencySort(String s) {
        String str="";
-	  for(Map.Entry<Character,Integer> m2:map1.entrySet())
-	  {
-		  //System.out.println(m2.getKey()+":"+m2.getValue());
-		  int c=1;
-		  while(c<=m2.getValue())
-		  {
-			  str=str+Character.toString(m2.getKey());
-			  c++;
-		  }
-	  }
-      StringBuilder s1=new StringBuilder(str);
-      return s1.reverse().toString();
+       HashMap<Character,Integer> map=new HashMap<>();
+       for(int i=0;i<s.length();i++)
+       {
+        map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+       } 
+
+       List<Map.Entry<Character,Integer>> list=new ArrayList<>(map.entrySet());
+       list.sort((a,b)->b.getValue()-a.getValue());//e-2,t-2,r-2
+       int c=1;
+       for(Map.Entry<Character,Integer> m:list)
+       {    
+        while(c<=m.getValue())
+        {
+            str+=Character.toString(m.getKey());
+            c++;
+        }
+        c=1;
+       }
+       return str;
     }
 }
