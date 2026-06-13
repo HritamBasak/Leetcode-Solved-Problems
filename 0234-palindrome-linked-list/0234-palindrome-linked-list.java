6880f866d3e370645a9e80b9546daa8cec3a9e93
Hritam_DEV <137753891+HritamBasak@users.noreply.github.com>
2026-06-13 21:02:12 +0530

    Time: 3 ms (99.78%), Space: 94.3 MB (65.79%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,68 +8,32 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution 
-{
-    public boolean isPalindrome(ListNode head) 
-    {
-        // Method 1:Firstly by t=copying the list and reversing the list
-    // ListNode copy = new ListNode(head.val);
-    // ListNode tempCopy = copy;
-    // ListNode tempOriginal = head.next;
-
-    // while (tempOriginal != null) 
-    // {
-    // tempCopy.next = new ListNode(tempOriginal.val);
-    // tempCopy = tempCopy.next;
-    // tempOriginal = tempOriginal.next;
-    // }
-
-    // // Now reverse the original list
-    // ListNode temp = head;
-    // ListNode prev = null;
-    // while (temp != null) 
-    // {
-    // ListNode cur = temp.next;
-    // temp.next = prev;
-    // prev = temp;
-    // temp = cur;
-    // }
-
-    // // Compare the copied list with the reversed list
-    // while (prev != null && copy != null) 
-    // {
-    // System.out.println(copy.val + " " + prev.val);
-    // if (copy.val != prev.val)
-    //     return false;
-    // copy = copy.next;
-    // prev = prev.next;
-    // }   
-    // return true;
-
-    //Method 2:By slow and fast pointers
-    ListNode slow=head;
-    ListNode fast=head;
-    while(fast!=null && fast.next!=null)
-    {
-        slow=slow.next;
-        fast=fast.next.next;
-    }
-    ListNode prev = null, curr = slow;
-    while (curr != null)
-    {
-        ListNode next1 = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = next1;
-    }
-    ListNode secondhalf=prev;
-    while(prev!=null)
-    {
-        if(prev.val!=head.val)
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode prev=null;
+        ListNode slow=head,fast=head;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode curr=slow.next;
+        slow.next=null;
+        while(curr!=null)
+        {
+            ListNode c=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=c;
+        }
+        while(head!=null && prev!=null)
+        {
+            if(head.val!=prev.val)
             return false;
-        prev=prev.next;
-        head=head.next;
-    }
-    return true;
+
+            head=head.next;
+            prev=prev.next;
+        }
+        return true;
     }
 }
